4d08784dea05eeac91a5d34b0b6a2e4ac948e3ab
bz1013 <hntek@yahoo.com>
2016-12-17 23:06:24 -0800

    small change for lab2 made

--- sd/sdse.py
+++ sd/sdse.py
@@ -2,7 +2,8 @@ import numpy as np
 def standard_deviation(x):
     x = [i for i in x if ~np.isnan(i)]
     n = len(x)
-    mean = sum(x) / n
+    denumerator = sum(x)
+    mean = denumerator / n
     ssq = sum((x_i-mean)**2 for x_i in x)
     stdev = (ssq/n)**0.5
     return(stdev)
